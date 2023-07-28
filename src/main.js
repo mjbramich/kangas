8d87f602a56414b335e8d03ffc13298ed445ed33
Matthew Bramich <matt_bramich@hotmail.com>
2023-07-28 10:44:59 +1000

    added services page

--- src/main.js
+++ src/main.js
@@ -1,42 +1,3 @@
-// Get the file input and the parent container
-const fileInput = document.getElementById('file-input');
-const parentContainer = document.getElementById('file-input-container');
-
-// Handle key press events on the file attachment input
-parentContainer.addEventListener('keydown', (event) => {
-	if (event.key === 'Enter') {
-		fileInput.click();
-	}
-});
-
-const privacyToggle = document.querySelector('#privacy-toggle-btn');
-const privacySlider = document.querySelector('#privacy-slider');
-
-console.log(privacyToggle);
-
-let privacyEnabled = false;
-
-privacyToggle.onclick = () => {
-	privacyEnabled = !privacyEnabled;
-	console.log(privacyToggle);
-	if (privacyEnabled) {
-		privacyToggle.classList.add('bg-blue');
-		privacyToggle.classList.remove('bg-dark-gray');
-		privacySlider.classList.add('translate-x-3.5');
-		privacySlider.classList.remove('translate-x-0');
-	} else {
-		privacyToggle.classList.remove('bg-blue');
-		privacyToggle.classList.add('bg-dark-gray');
-		privacySlider.classList.remove('translate-x-3.5');
-		privacySlider.classList.add('translate-x-0');
-	}
-};
-
-// Update year in the footer
-const yearText = document.getElementById('get-year');
-const currentYear = new Date().getFullYear();
-yearText.innerText = `${currentYear}`;
-
 // Hamburger menu
 const createMobileMenu = () => {
 	let isOpen = false;
@@ -121,3 +82,54 @@ const createMobileMenu = () => {
 const mobileMenuInstance = createMobileMenu();
 const navBtn = document.getElementById('navBtn');
 navBtn.addEventListener('click', () => mobileMenuInstance.toggleMobileMenu());
+
+const createPrivacyToggle = () => {
+	const fileInput = document.getElementById('file-input');
+	const parentContainer = document.getElementById('file-input-container');
+
+	// Handle key press events on the file attachment input
+	parentContainer.addEventListener('keydown', (event) => {
+		if (event.key === 'Enter') {
+			fileInput.click();
+		}
+	});
+
+	const privacyToggle = document.querySelector('#privacy-toggle-btn');
+	const privacySlider = document.querySelector('#privacy-slider');
+
+	console.log(privacyToggle);
+
+	let privacyEnabled = false;
+
+	privacyToggle.onclick = () => {
+		privacyEnabled = !privacyEnabled;
+		console.log(privacyToggle);
+		if (privacyEnabled) {
+			privacyToggle.classList.add('bg-blue');
+			privacyToggle.classList.remove('bg-dark-gray');
+			privacySlider.classList.add('translate-x-3.5');
+			privacySlider.classList.remove('translate-x-0');
+		} else {
+			privacyToggle.classList.remove('bg-blue');
+			privacyToggle.classList.add('bg-dark-gray');
+			privacySlider.classList.remove('translate-x-3.5');
+			privacySlider.classList.add('translate-x-0');
+		}
+	};
+};
+
+if (document.getElementById('#privacy-Toggle-Btn')) {
+	createPrivacyToggle();
+}
+
+// Update year in the footer
+
+const updateFooterYear = () => {
+	const yearText = document.getElementById('get-year');
+	const currentYear = new Date().getFullYear();
+	yearText.innerText = `${currentYear}`;
+};
+
+if (document.querySelector('footer')) {
+	updateFooterYear();
+}
