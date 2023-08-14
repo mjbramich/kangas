389dcafe230f9cfebaf4b6991024167e9091d049
Matthew Bramich <matt_bramich@hotmail.com>
2023-08-14 21:47:03 +1000

    added faq in contact section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// import faqToggle from './faqToggle';
+
 // Hamburger menu
 const createMobileMenu = () => {
 	let isOpen = false;
@@ -133,3 +135,42 @@ const updateFooterYear = () => {
 if (document.querySelector('footer')) {
 	updateFooterYear();
 }
+
+const faqToggle = () => {
+	const faqList = document.getElementById('faqList');
+	const questions = Array.from(faqList.querySelectorAll('dt'));
+
+	questions.forEach((question) => {
+		question.addEventListener('click', handleClick);
+	});
+
+	// need to loop over list
+	function handleClick(e) {
+		const clickedQuestion = e.currentTarget;
+
+		// get the corresponding answet to the clicked question
+		const answerId = clickedQuestion
+			.querySelector('[aria-controls]')
+			.getAttribute('aria-controls');
+
+		const answer = document.getElementById(answerId);
+
+		const openIcon = clickedQuestion.querySelector('#openIcon');
+		const closeIcon = clickedQuestion.querySelector('#closeIcon');
+
+		const isExpanded =
+			clickedQuestion
+				.querySelector('[aria-expanded]')
+				.getAttribute('aria-expanded') === 'true';
+
+		clickedQuestion
+			.querySelector('[aria-expanded]')
+			.setAttribute('aria-expanded', !isExpanded);
+
+		answer.classList.toggle('hidden');
+		openIcon.classList.toggle('hidden');
+		closeIcon.classList.toggle('hidden');
+	}
+};
+
+faqToggle();
