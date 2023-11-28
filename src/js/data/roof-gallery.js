90c6514a5949173813438e6a90b35f842d1166cc
Matthew Bramich <matt_bramich@hotmail.com>
2023-11-29 09:41:07 +1100

    update photoswipe implementation

---
+++ src/js/data/roof-gallery.js
@@ -0,0 +1,31 @@
+const roofData = [
+	{
+		src: '../images/beforeafters/roof/roof1.jpeg',
+		width: 1439,
+		height: 1800,
+		alt: 'Before and after roof clean',
+	},
+	{
+		src: '../images/beforeafters/roof/roof2.jpeg',
+		width: 1439,
+		height: 1800,
+		alt: 'Before and after roof clean',
+	},
+	{
+		src: 'https://dummyimage.com/800x600/555/fff/?text=3',
+		width: 800,
+		height: 600,
+	},
+	{
+		src: 'https://dummyimage.com/800x600/555/fff/?text=4',
+		width: 800,
+		height: 600,
+	},
+	{
+		src: 'https://dummyimage.com/800x600/555/fff/?text=5',
+		width: 800,
+		height: 600,
+	},
+];
+
+export default roofData;
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,17 +1,64 @@
 import 'photoswipe/style.css';
-import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import PhotoSwipe from 'photoswipe';
+import roofData from './data/roof-gallery';
+
+const options = {
+	dataSource: [
+		{
+			src: '../images/beforeafters/roof/roof1.jpeg',
+			width: 1439,
+			height: 1800,
+			alt: 'Before and after roof clean',
+		},
+		{
+			src: '../images/beforeafters/roof/roof2.jpeg',
+			width: 1439,
+			height: 1800,
+			alt: 'Before and after roof clean',
+		},
+
+		{
+			src: 'https://dummyimage.com/800x600/555/fff/?text=3',
+			width: 800,
+			height: 600,
+		},
+		{
+			src: 'https://dummyimage.com/800x600/555/fff/?text=4',
+			width: 800,
+			height: 600,
+		},
+		{
+			src: 'https://dummyimage.com/800x600/555/fff/?text=5',
+			width: 800,
+			height: 600,
+		},
+	],
+	pswpModule: () => import('photoswipe'),
+};
+
+const galleries = {
+	'roof-gallery': roofData,
+	'house-gallery': roofData,
+	'gutter-gallery': roofData,
+	'solarpanel-gallery': roofData,
+	'driveway-gallery': roofData,
+	'bandp-gallery': roofData,
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-	const lightbox = new PhotoSwipeLightbox({
-		gallery: '#my-gallery',
-		children: 'a',
-		pswpModule: () => import('photoswipe'),
-	});
-	lightbox.init();
+	function initializeGallery(galleryId, dataSource) {
+		document.querySelector(`#${galleryId}`).onclick = () => {
+			const options = {
+				dataSource: dataSource,
+				pswpModule: () => import('photoswipe'),
+				// Add other common options for all galleries
+			};
+			const lightbox = new PhotoSwipe(options);
+			lightbox.init();
+		};
+	}
 
-	lightbox.on('beforeOpen', (e) => {
-		console.log(e);
-		console.log('beforeOpen');
-		// photoswipe starts to open
-	});
+	for (const [galleryId, dataSource] of Object.entries(galleries)) {
+		initializeGallery(galleryId, dataSource);
+	}
 });
